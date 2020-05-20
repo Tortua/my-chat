734aa49707ff3cb33626a5072425c20d3aa61cb9
Tortua <dianajaborszki@gmail.com>
2020-05-20 21:21:51 +0200

    #build

---
+++ build/precache-manifest.c96229886f260d95f7cf971b3524e331.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9fb5834302db2610f2c5ac544065021d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "6c4e136c099b0ad34028",
+    "url": "/static/css/main.a72c4fbf.chunk.css"
+  },
+  {
+    "revision": "f9f1c85ed2dfb059483d",
+    "url": "/static/js/2.db39bea0.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.db39bea0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6c4e136c099b0ad34028",
+    "url": "/static/js/main.8df83d71.chunk.js"
+  },
+  {
+    "revision": "869c4e71bcc3d7697614",
+    "url": "/static/js/runtime-main.b9d4600e.js"
+  }
+]);
